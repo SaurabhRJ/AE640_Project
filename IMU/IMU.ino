1d0639cfe7acfc3add0f04b5ad0b9b29c0a27d4f
Saurabh <sranjan97@gmail.com>
2018-03-09 20:31:05 +0530

    Compiled IMU code

--- IMU/IMU.ino
+++ IMU/IMU.ino
@@ -1,108 +1,154 @@
 /*
-    MPU6050 Triple Axis Gyroscope & Accelerometer. Simple Accelerometer Example.
-    Read more: http://www.jarzebski.pl/arduino/czujniki-i-sensory/3-osiowy-zyroskop-i-akcelerometr-mpu6050.html
-    GIT: https://github.com/jarzebski/Arduino-MPU6050
-    Web: http://www.jarzebski.pl
-    (c) 2014 by Korneliusz Jarzebski
+  GY-87 Test
+  Tests basic functionality of the GY-87 sensor board.
+  
+  Connections:
+  |== GY-87 ==|== Arduino ==|
+  |  VCC_IN   |     VCC     |
+  |   GND     |     GND     |
+  |   SDA     |     SDA     |
+  |   SCL     |     SCL     |
+  |=========================|
+ 
+  Requires the I2CDevLib library, which can be found here: https://github.com/jrowberg/i2cdevlib
+  
+  by Tom Kuehn
+  26/06/2016
 */
-////////// All credit goes to author above with word of thanks.
-/*
-Wiring:
-This code works fine with GY87 IMU module. Connect gy87 module power lins to +5v and GND.
-and SDA & SCL pins to I2C pins on Arduino board. All you have to do is connect these four wires and you should
-start get data on serial monitor just by uploading this code.
-Arduino software version 1.6.0 has been used while compiling this code.
-Details at: http://hotresistor.blogspot.com
-*/
 
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "HMC5883L.h"
+#include "BMP085.h"
+#include "Wire.h"
 
-#include <Wire.h>  // for I2C library
-#include <MPU6050.h> //put this library folder in arduino-folder>libraries
+static const char LED = 6;
+static const float ACCEL_SENS = 16384.0; // Accel Sensitivity with default +/- 2g scale
+static const float GYRO_SENS  = 131.0;   // Gyro Sensitivity with default +/- 250 deg/s scale
 
-MPU6050 mpu;
+// Magnetometer class default I2C address is 0x1E
+// specific I2C addresses may be passed as a parameter here
+// this device only supports one I2C address (0x1E)
+HMC5883L mag;
+int16_t mx, my, mz;
 
-void setup() 
+// Accel/Gyro class default I2C address is 0x68 (can be 0x69 if AD0 is high)
+// specific I2C addresses may be passed as a parameter here
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+// Barometer class default I2C address is 0x77
+// specific I2C addresses may be passed as a parameter here
+// (though the BMP085 supports only one address)
+BMP085 barometer;
+
+float temperature;
+float pressure;
+int32_t lastMicros;
+
+void setup()
 {
-  Serial.begin(115200);
-
-  Serial.println("Initialize MPU6050");
-
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  boolean state = HIGH;
+  unsigned int count = 0;
+  
+  pinMode(LED, OUTPUT);
+  
+  Serial.begin(9600);
+  while (!Serial && (count < 30) )
   {
-    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
-    delay(500);
+    delay(200); // Wait for serial port to connect with timeout. Needed for native USB
+    digitalWrite(LED, state);
+    state = !state;
+    count++;
   }
 
-  // If you want, you can set accelerometer offsets
-  // mpu.setAccelOffsetX();
-  // mpu.setAccelOffsetY();
-  // mpu.setAccelOffsetZ();
-  
-  checkSettings();
-}
+  digitalWrite(LED, HIGH);
 
-void checkSettings()
-{
-  Serial.println();
-  
-  Serial.print(" * Sleep Mode:            ");
-  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
-  
-  Serial.print(" * Clock Source:          ");
-  switch(mpu.getClockSource())
-  {
-    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
-    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
-    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
-    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
-    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
-    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
-    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
-  }
-  
-  Serial.print(" * Accelerometer:         ");
-  switch(mpu.getRange())
-  {
-    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
-    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
-    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
-    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
-  }  
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  Wire.begin();
 
-  Serial.print(" * Accelerometer offsets: ");
-  Serial.print(mpu.getAccelOffsetX());
-  Serial.print(" / ");
-  Serial.print(mpu.getAccelOffsetY());
-  Serial.print(" / ");
-  Serial.println(mpu.getAccelOffsetZ());
-  
-  Serial.println();
+  // ==================== MPU6050 ============================
+  accelgyro.initialize();
+  Serial.print("Testing Accel/Gyro... ");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+  // Starts up with accel +/- 2 g and gyro +/- 250 deg/s scale
+  accelgyro.setI2CBypassEnabled(true); // set bypass mode
+  // Now we can talk to the HMC5883l
+
+  // ==================== HMC5883L ============================
+  mag.initialize();
+  Serial.print("Testing Mag...  ");
+  Serial.println(mag.testConnection() ? "HMC5883L connection successful" : "HMC5883L connection failed");
+
+  // ==================== BMP085 ============================
+  barometer.initialize();
+  Serial.print("Testing Pressure...  ");
+  Serial.println(barometer.testConnection() ? "BMP085 connection successful" : "BMP085 connection failed");
+
+  Serial.println("Setup Complete");
 }
 
 void loop()
 {
-  Vector rawAccel = mpu.readRawAccel();
-  Vector normAccel = mpu.readNormalizeAccel();
+  static unsigned long ms = 0;
+  static boolean state = HIGH;
 
-  Serial.print("\t");
-  Serial.print(rawAccel.XAxis);
-  Serial.print("\t ");
-  Serial.print(rawAccel.YAxis);
-  Serial.print(" \t ");
-  Serial.println(rawAccel.ZAxis);
-  
-  
-  
-//    Serial.print(" \t \t"); ///remove comments if you want to use any value.
-//  Serial.print(normAccel.XAxis);
-//    Serial.print(" \t ");
-//  Serial.print(normAccel.YAxis);
-//    Serial.print(" \t ");
-//  Serial.println(normAccel.ZAxis);
-  
-  delay(400);
+  // Serial Output Format
+  // === Accel === | === Gyro === | ======= Mag ======= | === Barometer === |
+  //   X   Y   Z   |  X   Y   Z   |  X   Y   Z  Heading |  Temp   Pressure  |
+
+  if (millis() - ms > 100)
+  {
+    // read raw accel/gyro measurements
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    // display tab-separated accel/gyro x/y/z values
+    Serial.print(ax/ACCEL_SENS); Serial.print("\t");
+    Serial.print(ay/ACCEL_SENS); Serial.print("\t");
+    Serial.print(az/ACCEL_SENS); Serial.print("\t");
+    Serial.print(gx/GYRO_SENS); Serial.print("\t");
+    Serial.print(gy/GYRO_SENS); Serial.print("\t");
+    Serial.print(gz/GYRO_SENS); Serial.print("\t");
+
+    // read raw heading measurements
+    mag.getHeading(&mx, &my, &mz);
+
+    // display tab-separated mag x/y/z values
+    Serial.print(mx); Serial.print("\t");
+    Serial.print(my); Serial.print("\t");
+    Serial.print(mz); Serial.print("\t");
+    
+    // To calculate heading in degrees. 0 degree indicates North
+    float heading = atan2(my, mx);
+    if(heading < 0) heading += 2 * M_PI;
+    Serial.print(heading * 180/M_PI); Serial.print("\t");
+
+    // request temperature
+    barometer.setControl(BMP085_MODE_TEMPERATURE);
+    
+    // wait appropriate time for conversion (4.5ms delay)
+    lastMicros = micros();
+    while (micros() - lastMicros < barometer.getMeasureDelayMicroseconds());
+
+    // read calibrated temperature value in degrees Celsius
+    temperature = barometer.getTemperatureC();
+
+    // request pressure (3x oversampling mode, high detail, 23.5ms delay)
+    barometer.setControl(BMP085_MODE_PRESSURE_3);
+    while (micros() - lastMicros < barometer.getMeasureDelayMicroseconds());
+
+    // read calibrated pressure value in Pascals (Pa)
+    pressure = barometer.getPressure();
+
+    // display measured values if appropriate
+    Serial.print(temperature); Serial.print("\t");
+    Serial.print(pressure/100); Serial.println("\t");
+
+    ms = millis();
+    digitalWrite(LED, state);
+    state = !state;
+  }
 }
-
-
-
-
